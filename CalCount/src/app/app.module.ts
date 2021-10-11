79ac143c48549c8e68903ecc85b9af7ff79d29ce
jonathan.powers <jonathan.powers@revature.net>
2021-10-11 16:15:26 -0400

    Update

--- CalCount/src/app/app.module.ts
+++ CalCount/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
+const routes: Routes = []
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
