29c43eaa4a320b374edc9d5976fb728720c2963e
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-17 01:37:09 -0700

    Reuploading this because this was in its separate file for some reason

--- CalCount/src/app/components/create-account/create-account.component.ts
+++ CalCount/src/app/components/create-account/create-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { CreateAccountService } from 'src/app/services/create-account.service';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(private createAccountService:CreateAccountService) { }
 
   ngOnInit(): void {
   }
 
+  user: User = {
+    "id": 1,
+    "name": "name",
+    "email": "email@email.com",
+    "password": "password",
+    "height": 150,
+    "weight": 50,
+    "friends": [],
+};
+  
+  createUser() {
+    let new_user: User = this.user; //this may be a bit sketch
+      this.createAccountService.createUser(new_user).subscribe(
+        (data) => {
+          this.user = data; //this is just a placeholder, maybe this can be deleted?
+        }
+      )
+  }
 }
