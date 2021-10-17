650b8fa72b87fe152a87e40519b6872acee41f18
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-17 01:26:54 -0700

    Added new http requests paths

--- CalCount/src/environments/environment.prod.ts
+++ CalCount/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   findCredentials: "http://localhost:8000/users/login", //CHANGE THIS ONE
-  createUser: "http://localhost:8000/users/new" //CHANGE THIS ONE
+  createUser: "http://localhost:8000/users/new", //CHANGE THIS ONE!!!
+  findUser: "http://localhost:8000/users/{id}}" //CHANGE THIS ONE!!
 };
