343b1b2de2340e8718ba5867c3fae2563d4f33be
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-16 22:58:15 -0700

    turned this into an interface

--- CalCount/src/app/models/User.ts
+++ CalCount/src/app/models/User.ts
@@ -1,29 +1,9 @@
-export class User{
-    constructor(private name:string, private email:string, private password:string, 
-        private height:number, private weight:number) {
-            this.name = name;
-            this.email = email;
-            this.password = password;
-            this.height = height;
-            this.weight = weight;
-        }
-    getName(){
-        return this.name
-    }
-
-    getEmail(){
-        return this.email
-    }
-
-    getPassword(){
-        return this.password
-    }
-
-    getHeight(){
-        return this.height
-    }
-
-    getWeight(){
-        return this.weight
-    }
-}
+export interface User {
+    id: number;
+    email: string;
+    name: string;
+    password: string;
+    height: number;
+    weight: number;
+    friends: number[];
+  }
