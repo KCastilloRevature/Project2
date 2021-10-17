2928f4193fd93a0d337eac67b09e3c191b2ee33e
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-17 01:50:23 -0700

    Pushing my wotk into development

--- CalCount/src/app/login/login.component.ts
+++ CalCount/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { User } from '../models/User';
+import { Component, OnInit, Output } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { LoginService } from 'src/app/services/login.service';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +10,44 @@ import { User } from '../models/User';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService) { 
+  }
+
+  user: User = {
+    "name": "name",
+    "email": "email@email.com",
+    "password": "password",
+    "height": 150,
+    "weight": 50,
+    "friends": [],
+    "id": 1
+};
+  updatedUser: User = {
+    "name": "name",
+    "email": "email@email.com",
+    "password": "password",
+    "height": 150,
+    "weight": 50,
+    "friends": [],
+    "id": 1
+};
 
   ngOnInit(): void {
   }
 
   @Output()
   updateUserEvent = new EventEmitter<User>();
+  
+  findCredentials(){
+    this.loginService.findCredentials().subscribe(
+      (data: User) => {
+        this.user = data;
+      }
+    )
+  }
+
+  updateUserState(){
+    this.updateUserEvent.emit(this.user);
+  }
 }
+
